3868e84c7a188313c938dbcaffa64e93245b3007
Koffi Nyonouglo <nyonouglokoffi8@gmail.com>
2020-08-25 01:30:09 +0000

    Android Music Player
    This is a simple android music player with some cool slidng panel up and down to entatain users.The app is on playstore.

---
+++ music player/app/build/generated/source/buildConfig/release/com/messieyawo/musicplayer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.messieyawo.musicplayer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.messieyawo.musicplayer";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 61;
+  public static final String VERSION_NAME = "1.0.2";
+}
